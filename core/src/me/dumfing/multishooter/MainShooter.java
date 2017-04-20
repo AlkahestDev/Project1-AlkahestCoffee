e6a04b83560820908c332e6c7fe2631b580e72fa
Dumfing <dumpling.d.m@gmail.com>
2017-04-20 00:10:04 -0400

    animated loading gif
    we can use any series of images as a loading icon, here I used tuzki

--- core/src/me/dumfing/multishooter/MainShooter.java
+++ core/src/me/dumfing/multishooter/MainShooter.java
@@ -7,6 +7,7 @@ import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import me.dumfing.multiplayerTools.MultiplayerClient;
 import me.dumfing.multiplayerTools.MultiplayerTools;
 
@@ -17,14 +18,15 @@ public class MainShooter extends ApplicationAdapter implements InputProcessor{
 	SpriteBatch batch;
 	AssetManager assetManager;
 	GameState state;
-	Texture loadingIcon1, tuzki, menuImg;
+	Texture tuzki, menuImg;
+	Texture[] loadingFrames;
 	MultiplayerClient player;
 	MultiplayerTools.PlayerSoldier clientSoldier;
+    int loadingFrame, frameCount;
 
 	private enum GameState{
-		LOADINGGAME, // loading all assets into the assetmanager
+		LOADINGGAME, // loading all assets into the assetmanager and place them into variables
 		MAINMENU, // Main menu with play, settings, and quit
-		ASSIGNTEXTURES, // assigning variables from loaded assets in the assetmanager
 		MAINMENUSETTINGS, // settings for the game
 		SERVERBROWSER, // browse all active servers on the network
 			STARTSERVER, // start your own server on the network
@@ -41,15 +43,20 @@ public class MainShooter extends ApplicationAdapter implements InputProcessor{
 	}
 	@Override
 	public void create () {
+	    loadingFrame = frameCount = 0;
+	    int numFrames = 14;
+	    loadingFrames = new Texture[numFrames];
+	    for(int i = 1; i<numFrames+1;i++){
+	        loadingFrames[i-1] = new Texture(Gdx.files.internal(String.format("loading/tuzkiii%d.png",i)));
+        }
 		scW = Gdx.graphics.getWidth();
 		scH = Gdx.graphics.getHeight();
 		state = GameState.LOADINGGAME;
-		Scanner uIn = new Scanner(System.in);
 		batch = new SpriteBatch();
 		assetManager = new AssetManager();
 		assetManager.load("tuzki.png",Texture.class);
 		assetManager.load("Desktop.jpg",Texture.class);
-		loadingIcon1 = new Texture(Gdx.files.internal("tuzki.png"));
+
 		player = new MultiplayerClient();
 		player.startClient();
 	}
@@ -60,17 +67,20 @@ public class MainShooter extends ApplicationAdapter implements InputProcessor{
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		switch(state){
 			case LOADINGGAME: // we shouldn't ever be going back to LOADINGGAME
-				if(assetManager.update()) {
-					state = GameState.ASSIGNTEXTURES;
+                //LOADINGGAME just needs to call assetManager.update(), then assign the values
+				if(assetManager.update()) { // returns true if done loading
+                    //assets are assigned to variables here
+                    tuzki = assetManager.get("tuzki.png");
+                    menuImg = assetManager.get("Desktop.jpg");
+					state = GameState.MAINMENU;
 				}
+
 				batch.begin();
-					batch.draw(loadingIcon1,scW/2- loadingIcon1.getWidth()/2,scH/2- loadingIcon1.getHeight()/2);
+					batch.draw(loadingFrames[loadingFrame],scW/2- loadingFrames[loadingFrame].getWidth()/2,scH/2- loadingFrames[loadingFrame].getHeight()/2); // draw tuzki frame at center of screen
 				batch.end();
-				break;
-			case ASSIGNTEXTURES: // we should only go to ASSIGNTEXTURES once
-				tuzki = assetManager.get("tuzki.png");
-				menuImg = assetManager.get("Desktop.jpg");
-				state = GameState.MAINMENU;
+				if(frameCount % 2 == 0) { // change frame at 30fps (render is called at 60hz)
+                    loadingFrame = (1 + loadingFrame) % loadingFrames.length; // increase frame by 1, setting it to 0 if it goes over the number of frames
+                }
 				break;
 			case MAINMENU:
 				batch.begin();
@@ -96,6 +106,7 @@ public class MainShooter extends ApplicationAdapter implements InputProcessor{
 			case QUIT:
 				break;
 		}
+        frameCount = (1 + frameCount) % Integer.MAX_VALUE; // increase frameCount by 1, setting it to 0 if it is above integer.max_value
 	}
 
 	@Override
